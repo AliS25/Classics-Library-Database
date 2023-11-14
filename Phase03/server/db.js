bfc6b471991aa4edcef6bbad73ba81d4f11eb291
Hamza Shemsu <hshemsu10@gmail.com>
2023-11-14 04:31:54 -0500

    update db.js

--- Phase03/server/db.js
+++ Phase03/server/db.js
@@ -3,7 +3,7 @@ const Pool = require('pg').Pool;
 const pool = new Pool({
     user: 'postgres',
     password: 'classicslib',
-    host: 'localhost',
+    host: 'cosc-257-node08.cs.amherst.edu',
     port: 5432,
     database: 'library'
 });
